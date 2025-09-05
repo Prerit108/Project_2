bdd093ab5561a2361db06414f88da15c5da28117
Prerit Sharma <cloudsharma909@gmail.com>
2025-09-05 18:59:36 +0530

    basic setup

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+
+
+python-dotenv ##.env file is to store environment variables such as API keys, usernames, or passwords. Use the python-dotenv library to access these variables in your code.
---
+++ setup.py
@@ -0,0 +1,47 @@
+
+"""The setup. py file is an essential part of packaging and
+distributing Python projects. It is used by setuptools
+(or distutils in older Python versions) to define the configuration
+of your project, such as its metadata, dependencies, and more"""
+
+from typing import List
+
+## List is used for type hinting, especially when you want to specify that a variable, function argument, or return value is a list containing elements of a specific type.
+
+
+from setuptools import find_packages,setup
+## find_packages find the __init__.py files and consider the folder as a package
+## setup will provide all the information regarding the project
+
+def get_requirements()->List[str]:
+    "this function will return list of requirements"
+    requirement_list = []
+
+
+    try:
+        with open("requirements.txt","r") as  file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                
+                ## "-e ." is responsible for triggering the setup.py and build all packages
+                ## ignoring the empty lines and "-e ."
+                if requirement and requirement != "-e .":
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_list
+
+print(get_requirements())
+
+
+setup(
+    name = "mlproject2",
+    version = "0.0.1",
+    author = "Prerit",
+    author_email="cloudsharma909@gmail.com",
+    packages = find_packages(),  ## find all the packages in the project
+    install_requires = get_requirements()  ## Installs all the libraries
+)
+
